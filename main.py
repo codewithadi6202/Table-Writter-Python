07ae067102f3487355d2c1f34978000de2707ba9
CodWithAdi <infosystem6202@gmail.com>
2021-09-30 14:41:20 +0530

    Python Table Writter

---
+++ main.py
@@ -0,0 +1,8 @@
+number = int(input ("Enter the number of Table:")) # Getting Number of Table
+
+print ("The Multiplication Table of :- ", number) # Printing the info
+
+# Uisng of For Loop to Iterate the values of Table and Printing the multiplication Table
+
+for count in range(1, 11):      
+   print (number, 'x', count, '=', number * count)  
